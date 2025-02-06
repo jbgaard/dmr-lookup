6968a1a68352a02e77e3aaaee09382e8a363a17b
jbgaard <jxras11@hotmail.com>
2025-02-06 16:00:54 +0100

    mf not working

--- appconstants.ts
+++ appconstants.ts
@@ -1,3 +1,3 @@
 // TODO: Make this more dynamic, works for now.
-export const VERSION = '1.0.1';
+export const VERSION = '1.0.2';
 export const DMR_SERVICE_URL = 'https://bil.bilfeber.dk';
--- dist/appconstants.js
+++ dist/appconstants.js
@@ -2,5 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DMR_SERVICE_URL = exports.VERSION = void 0;
 // TODO: Make this more dynamic, works for now.
-exports.VERSION = '1.0.1';
+exports.VERSION = '1.0.2';
 exports.DMR_SERVICE_URL = 'https://bil.bilfeber.dk';
