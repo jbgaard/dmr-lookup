86af2cb8c5fd6cc0501c37d8f025fd2b7d050d3b
jbgaard <jxras11@hotmail.com>
2025-02-21 09:34:01 +0100

    fix: more information if found in DMR, also added link to external website to read more

--- index.ts
+++ index.ts
@@ -64,12 +64,20 @@ export function PrettyPrint(info:VehicleInfo, raw:boolean = false) {
 
     // Print vehicle info
     // console.log(chalk.bgGreen.bold('          === Køretøj informationer ===          '));
-    console.log(chalk.bgBlack.bold(`Mærke/Model:          %s`), info.køretøj.køretøj.mærkeModel);
-    console.log(chalk.gray(`Motor:               `), info.teknisk.motor.drivkraft);
+    console.log(chalk.bold(`Mærke/Model:          %s`), info.køretøj.køretøj.mærkeModel);
+    console.log(chalk.gray(`Motor:               `), 
+        info.teknisk.motor.drivkraft != undefined ? info.teknisk.motor.drivkraft : "", 
+        info.teknisk.motor.effekt != undefined && info.teknisk.motor.effekt != 0 ? `${((info.teknisk.motor.effekt / 10 ) / 0.7355).toFixed(0) }hk` : "", 
+        info.teknisk.motor.slagVolumen != undefined && info.teknisk.motor.slagVolumen != 0 ? `${info.teknisk.motor.slagVolumen}cc` : "", 
+        info.teknisk.motor.cylindre != undefined && info.teknisk.motor.cylindre != 0 ? `${info.teknisk.motor.cylindre} cylindre` : ""
+    );
     console.log(chalk.gray(`Registreringsnummer: `), info.køretøj.registreringsforhold.registreringsNummer);
     console.log(chalk.gray(`Stelnummer:          `), info.køretøj.køretøj.stelnummer);
     console.log(chalk.gray(`Art:                 `), info.køretøj.køretøj.art);
     console.log(chalk.gray(`Første registrering: `), info.køretøj.registreringsforhold.førsteRegistrering != null ? DateTime.fromJSDate(new Date(info.køretøj.registreringsforhold.førsteRegistrering)).toFormat("dd. LLL. yyyy") : 'N/A');
     console.log(chalk.gray(`Status:              `), info.køretøj.registreringsforhold.status);
+    
+    if (info.køretøj.registreringsforhold.registreringsNummer != null)
+        console.log(chalk.gray(`Læs mere:            `), `https://bilopslag.nu/nummerplade/${info.køretøj.registreringsforhold.registreringsNummer}`);
 
 }
