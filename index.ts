39b1708ac876e830fde1e5fb4d93313feb9b831d
jbgaard <jonas@jbgaard.dk>
2025-03-04 21:30:16 +0100

    prettier using zed - added bette errors

--- index.ts
+++ index.ts
@@ -1,83 +1,129 @@
 import { getVehicleInfo, getVehicleInfoByVin, VehicleInfo } from "./api.js";
-import chalk from 'chalk';
+import chalk from "chalk";
 import { VERSION } from "./appconstants.js";
-import ora from 'ora';
-import pkg from 'ts-luxon';
+import ora from "ora";
+import pkg from "ts-luxon";
 const { DateTime } = pkg;
 
 export function main() {
+  /* Get input parameters */
+  const args = process.argv.slice(2);
 
-    /* Get input parameters */
-    const args = process.argv.slice(2);
+  /* Get the command */
+  const command = args[0];
 
-    /* Get the command */
-    const command = args[0];
+  /* Get the command arguments */
+  const commandArgs = args.slice(1);
 
-    /* Get the command arguments */
-    const commandArgs = args.slice(1);
+  // Switch
+  switch (command) {
+    case "--version":
+    case "-v":
+      console.log(VERSION);
+      break;
+    case "--vin":
+      getVehicleInfoByVin(commandArgs[0]);
+      break;
+    default:
+      // If empty command
+      if (command == null || command == "") {
+        console.log("No command specified. V.", VERSION);
+        process.exit(1);
+      }
 
-    // Switch
-    switch (command) {
-        case '--version':
-        case '-v':
-            console.log(VERSION);
-            break;
-        case '--vin':
-            getVehicleInfoByVin(commandArgs[0]);
-            break;
-        default:
-            // If empty command
-            if (command == null || command == '') {
-                console.log('No command specified. V.', VERSION);
-                process.exit(1);
-            }
+      // Start spinner
+      const spinner = ora("Henter køretøjsinformationer").start();
 
-            // Start spinner
-            const spinner = ora("Henter køretøjsinformationer").start();
+      // Get vehicle info
+      getVehicleInfo(command)
+        .then((vehicleInfo) => {
+          // Check if --raw flag is set
+          const raw = commandArgs.includes("--raw");
 
-            // Get vehicle info
-            getVehicleInfo(command).then((vehicleInfo) => {
+          // Stop spinner
+          spinner.succeed(`Køretøjet med nummerplade ${command} er fundet.`);
 
-                // Check if --raw flag is set
-                const raw = commandArgs.includes('--raw');
+          // Pretty print vehicle info
+          PrettyPrint(vehicleInfo, raw);
+        })
+        .catch((error: Error) => {
+          // console.error(error);
 
-                // Stop spinner
-                spinner.stop();
+          // If error is 404, print error message
+          if (error.message.includes("404")) {
+            spinner.fail(`Køretøjet med nummerplade ${command} findes ikke.`);
+            return;
+          }
 
-                // Pretty print vehicle info
-                PrettyPrint(vehicleInfo, raw);
-            }).catch((error:Error) => {
-                console.error(error);
-            });
-            break;
-    }
+          // If error is 401, print error message
+          if (error.message.includes("401")) {
+            spinner.fail("Ugyldig API nøgle.");
+            return;
+          }
+
+          spinner.fail(
+            `Kunne ikke hente køretøjsinformationer: ${error.message}`,
+          );
+        });
+      break;
+  }
 }
 
 // Pretty print
-export function PrettyPrint(info:VehicleInfo, raw:boolean = false) {
+export function PrettyPrint(info: VehicleInfo, raw: boolean = false) {
+  // If raw, print raw JSON
+  if (raw) {
+    console.log(JSON.stringify(info, null, 2));
+    return;
+  }
 
-    // If raw, print raw JSON
-    if (raw) {
-        console.log(JSON.stringify(info, null, 2));
-        return
-    }
+  // Print vehicle info
+  // console.log(chalk.bgGreen.bold('          === Køretøj informationer ===          '));
+  console.log(
+    chalk.bold(`Mærke/Model:          %s`),
+    info.køretøj.køretøj.mærkeModel,
+  );
+  console.log(
+    chalk.gray(`Motor:               `),
+    info.teknisk.motor.drivkraft != undefined
+      ? info.teknisk.motor.drivkraft
+      : "",
+    info.teknisk.motor.effekt != undefined && info.teknisk.motor.effekt != 0
+      ? `${(info.teknisk.motor.effekt / 10 / 0.7355).toFixed(0)}hk`
+      : "",
+    info.teknisk.motor.slagVolumen != undefined &&
+      info.teknisk.motor.slagVolumen != 0
+      ? `${info.teknisk.motor.slagVolumen}cc`
+      : "",
+    info.teknisk.motor.cylindre != undefined && info.teknisk.motor.cylindre != 0
+      ? `${info.teknisk.motor.cylindre} cylindre`
+      : "",
+  );
+  console.log(
+    chalk.gray(`Registreringsnummer: `),
+    info.køretøj.registreringsforhold.registreringsNummer,
+  );
+  console.log(
+    chalk.gray(`Stelnummer:          `),
+    info.køretøj.køretøj.stelnummer,
+  );
+  console.log(chalk.gray(`Art:                 `), info.køretøj.køretøj.art);
+  console.log(
+    chalk.gray(`Første registrering: `),
+    info.køretøj.registreringsforhold.førsteRegistrering != null
+      ? DateTime.fromJSDate(
+          new Date(info.køretøj.registreringsforhold.førsteRegistrering),
+        ).toFormat("dd. LLL. yyyy")
+      : "N/A",
+  );
+  console.log(
+    chalk.gray(`Status:              `),
+    info.køretøj.registreringsforhold.status,
+  );
 
-    // Print vehicle info
-    // console.log(chalk.bgGreen.bold('          === Køretøj informationer ===          '));
-    console.log(chalk.bold(`Mærke/Model:          %s`), info.køretøj.køretøj.mærkeModel);
-    console.log(chalk.gray(`Motor:               `), 
-        info.teknisk.motor.drivkraft != undefined ? info.teknisk.motor.drivkraft : "", 
-        info.teknisk.motor.effekt != undefined && info.teknisk.motor.effekt != 0 ? `${((info.teknisk.motor.effekt / 10 ) / 0.7355).toFixed(0) }hk` : "", 
-        info.teknisk.motor.slagVolumen != undefined && info.teknisk.motor.slagVolumen != 0 ? `${info.teknisk.motor.slagVolumen}cc` : "", 
-        info.teknisk.motor.cylindre != undefined && info.teknisk.motor.cylindre != 0 ? `${info.teknisk.motor.cylindre} cylindre` : ""
+  if (info.køretøj.registreringsforhold.registreringsNummer != null)
+    console.log(
+      chalk.gray(`Læs mere:            `),
+      `https://bilopslag.nu/nummerplade/${info.køretøj.registreringsforhold.registreringsNummer}`,
     );
-    console.log(chalk.gray(`Registreringsnummer: `), info.køretøj.registreringsforhold.registreringsNummer);
-    console.log(chalk.gray(`Stelnummer:          `), info.køretøj.køretøj.stelnummer);
-    console.log(chalk.gray(`Art:                 `), info.køretøj.køretøj.art);
-    console.log(chalk.gray(`Første registrering: `), info.køretøj.registreringsforhold.førsteRegistrering != null ? DateTime.fromJSDate(new Date(info.køretøj.registreringsforhold.førsteRegistrering)).toFormat("dd. LLL. yyyy") : 'N/A');
-    console.log(chalk.gray(`Status:              `), info.køretøj.registreringsforhold.status);
-    
-    if (info.køretøj.registreringsforhold.registreringsNummer != null)
-        console.log(chalk.gray(`Læs mere:            `), `https://bilopslag.nu/nummerplade/${info.køretøj.registreringsforhold.registreringsNummer}`);
-
 }
